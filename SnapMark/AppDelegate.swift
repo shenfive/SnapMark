5c29e0af1840d0b0528a86c86a00de2933eb9bfa
Danny Shen <shenfive@gmail.com>
2025-07-28 16:25:17 +0800

    Initial Commit

---
+++ SnapMark/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SnapMark
+//
+//  Created by Danny Shen on 2025/7/28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
